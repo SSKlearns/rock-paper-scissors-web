8444fc408dd0b1a849fb7ac5a34cb97fd8d60bb0
SSK0908 <sarveshshashikumar0908@gmail.com>
2023-03-25 10:17:49 +0530

    Make buttons global var

--- gameLogic.js
+++ gameLogic.js
@@ -4,6 +4,7 @@ const rock = document.querySelector("#rock");   // Get the rock button
 const paper = document.querySelector("#paper"); // Get the paper button
 const scissor = document.querySelector("#scissor"); // Get the scissor button
 const restart = document.querySelector("#restart"); // Get the restart button
+const buttons = document.querySelectorAll('button'); // Get all the buttons
 var compChoice;         // Variable to store the computer choice
 var userScore = 0;      // Variable to store user score
 var compScore = 0;      // Variable to store computer score
@@ -12,7 +13,7 @@ updateScore();
 
 // Create a random number between a given range
 function getRandInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Choose rock, paper or scissor based on the random number 
@@ -45,31 +46,31 @@ function roundCheck(userInput, compChoice) {
     switch (compChoice) {
         case "rock":
             if (userInput === "paper") {
-                userScore ++;
+                userScore++;
                 document.getElementById("choicestat").innerHTML = "Paper defeats Rock";
             }
             else {
-                compScore ++;
+                compScore++;
                 document.getElementById("choicestat").innerHTML = "Scissor loses to Rock";
             }
             break;
         case "paper":
             if (userInput === "scissor") {
-                userScore ++;
+                userScore++;
                 document.getElementById("choicestat").innerHTML = "Scissor defeats Paper";
             }
             else {
-                compScore ++;
+                compScore++;
                 document.getElementById("choicestat").innerHTML = "Rock loses to Paper";
             }
             break;
         case "scissor":
             if (userInput === "rock") {
-                userScore ++;
+                userScore++;
                 document.getElementById("choicestat").innerHTML = "Rock defeats Scissor";
             }
             else {
-                compScore ++;
+                compScore++;
                 document.getElementById("choicestat").innerHTML = "Paper loses to Scissor";
             }
             break;
@@ -88,27 +89,26 @@ function disableButtons() {
     buttons.forEach(button => button.disabled = true);
     buttons.forEach(button => button.hover = false);
     buttons.forEach(button => button.classList.add("disabled"));
-  }
-  
-  // Enable all buttons
-  function enableButtons() {
-    const buttons = document.querySelectorAll('button');
+}
+
+// Enable all buttons
+function enableButtons() {
     buttons.forEach(button => button.disabled = false);
     buttons.forEach(button => button.classList.remove("disabled"));
-  }
-  
-  // Wait for the restart button to be clicked
-  function waitForRestart() {
+}
+
+// Wait for the restart button to be clicked
+function waitForRestart() {
     return new Promise(resolve => {
         const button = document.getElementById('restart');
         button.addEventListener('click', () => {
             resolve();
         });
     });
-  }
-  
-  // Check if the game has ended
-  async function checkEnd() {
+}
+
+// Check if the game has ended
+async function checkEnd() {
     if (userScore === 5) {
         disableButtons();
         document.getElementById("choicestat").innerHTML = "You win!";
@@ -116,8 +116,8 @@ function disableButtons() {
         userScore = 0;
         compScore = 0;
         enableButtons();
-    } 
-    
+    }
+
     else if (compScore === 5) {
         disableButtons();
         document.getElementById("choicestat").innerHTML = "You lose!";
@@ -126,8 +126,8 @@ function disableButtons() {
         compScore = 0;
         enableButtons();
     }
-  }
-  
+}
+
 // Ask for user choice
 function game(userInput) {
     getCompChoice();
@@ -137,21 +137,21 @@ function game(userInput) {
 }
 
 // Add event listeners to the buttons
-rock.addEventListener("click", function() {
+rock.addEventListener("click", function () {
     game("rock");
 }
 );
-paper.addEventListener("click", function() {
+paper.addEventListener("click", function () {
     game("paper");
 }
 );
-scissor.addEventListener("click", function() {
+scissor.addEventListener("click", function () {
     game("scissor");
 }
 );
 
 // Restart the game
-restart.addEventListener("click", function() {
+restart.addEventListener("click", function () {
     userScore = 0;
     compScore = 0;
     updateScore();
